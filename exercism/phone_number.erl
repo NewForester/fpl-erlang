3019beca55fc03d1f0de93eaee0d30bbac2658d4
Paul Bryan Roberts <NewForester@users.noreply.github.com>
2017-11-29 23:55:39 +0000

    Add exercism exercise #19, iteration #2 - Phone Number

--- exercism/phone_number.erl
+++ exercism/phone_number.erl
@@ -3,45 +3,31 @@
 -export([number/1, areacode/1, pretty_print/1, test_version/0]).
 
 number(String) ->
-    number(lists:reverse(String), 0, []).
+    numb([C || C <- String, C >= $0, C =< $9]).
 
-number([$1], 10, N) ->
-    N;
-number([], 10, N) ->
-    N;
-number([], _, _) ->
-    "0000000000";
-number([H|T], C, N) when H >= $0, H =< $9 ->
-    number(T, C + 1, [H | N]);
-number([_|T], C, N) ->
-    number(T, C, N).
+numb([$1|String]) when length(String) == 10 ->
+    String;
+numb(String) when length(String) == 10 ->
+    String;
+numb(String) ->
+    string:copies("0", 10).
 
 areacode(String) ->
     string:left(number(String), 3).
 
 pretty_print(String) ->
-    pretty_print(number(String), 0, []).
+    [A1, A2, A3, B1, B2, B3, C1, C2, C3, C4] = number(String),
 
-pretty_print([], 10, PP) ->
-    lists:reverse(PP);
-pretty_print([H|T], 0, PP) ->
-    pretty_print(T, 1, [H | [$( | PP]]);
-pretty_print([H|T], 3, PP) ->
-    pretty_print(T, 4, [H | [$\s | [$) | PP]]]);
-pretty_print([H|T], 6, PP) ->
-    pretty_print(T, 7, [H | [$- | PP]]);
-pretty_print([H|T], C, PP) ->
-    pretty_print(T, C + 1, [H | PP]).
+    [$(, A1, A2, A3, $), $\s, B1, B2, B3, $-, C1, C2, C3, C4].
 
 test_version() -> 1.
 
 %%
-%% This iteration is pretty erlang-like because of all the recursion but,
-%% as expected most other solutions do a better job.
+%% After examination of other solutions.
 %%
-%% One could use regular expressions but this is erlang.
+%% Using a list comprehension for number is something I should have done
+%% without looking at other solutions.
 %%
-%% See iteration #2 for better ways of doing number/3 and pretty_print/3.
-%%
-%% As for the simple areacode, I could have used string:split/3 or string:substr/3.
+%% The solution for pretty_print I would never have thought of.  Pity
+%% because that is exactly what Erlang is good at.
 %%

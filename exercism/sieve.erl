449ae3b52a3bf1804cc9d6534215f63f7f8ec479
Paul Bryan Roberts <NewForester@users.noreply.github.com>
2018-01-14 19:37:51 +0000

    Add solution to exercism Sieve exercise, iteration #2

--- exercism/sieve.erl
+++ exercism/sieve.erl
@@ -1,20 +1,22 @@
--module(example).
+-module(sieve).
 
 -export([sieve/1, test_version/0]).
 
 sieve(N) when N < 2 ->
     [];
 sieve(N) ->
-    sieve(lists:seq(2,N), []).
+    sieve(lists:seq(2,N), [], math:sqrt(N)).
 
-sieve([], P) ->
+sieve([], P, _) ->
     lists:reverse(P);
-sieve([N|S], P) ->
+sieve([N|S], P, L) when N < L ->
     R = lists:filter(fun(X) -> X rem N /= 0 end, S),
-    sieve(R, [N|P]).
+    sieve(R, [N|P], L);
+sieve([N|S], P, L) ->
+    sieve(S, [N|P], L).
 
 test_version() -> 1.
 
 %%
-%% Example solution submitted to the Exercism Erlang stack.
+%%  Add square root optimisation
 %%

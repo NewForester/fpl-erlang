24e8a5988891ec7ccc7edba91b51e2ca6243b874
Paul Bryan Roberts <NewForester@users.noreply.github.com>
2018-02-21 11:32:52 +0000

    Add solution to exercism Raindrops exercise, iteration #2

--- exercism/raindrops.erl
+++ exercism/raindrops.erl
@@ -2,37 +2,17 @@
 
 -export([convert/1, test_version/0]).
 
-convert(Number) when Number rem 3 == 0 ->
-    pling(Number);
-convert(Number) when Number rem 5 == 0 ->
-    plang(Number);
-convert(Number) when Number rem 7 == 0 ->
-    plong(Number);
+sounds() -> [{3, "Pling"}, {5, "Plang"}, {7, "Plong"}].
+
 convert(Number) ->
-    integer_to_list(Number).
+  Drops = [Sound || {Factor, Sound} <- sounds(), Number rem Factor =:= 0],
+  case Drops of
+    [] -> integer_to_list(Number);
+    _ -> lists:concat(Drops)
+  end.
 
-pling(Number) when Number rem 5 == 0 ->
-    "Pling" ++ plang(Number);
-pling(Number) when Number rem 7 == 0 ->
-    "Pling" ++ plong(Number);
-pling(_) ->
-    "Pling".
-
-plang(Number) when Number rem 7 == 0 ->
-    "Plang" ++ plong(Number);
-plang(_) ->
-    "Plang".
-
-plong(_) ->
-    "Plong".
-
-test_version() ->
-    1.
+test_version() -> 1.
 
 %%
-%% This was part of my contribution to the Exercism Erlang branch.
-%%
-%% Yes, it was intended to be a simple, brute force solution.
-%% Yes, it is more complex than anticipated.
-%% Yes, the first submission by someone else was simpler.
+%% A generic solution inspired by others.
 %%

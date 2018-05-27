817ae9115ee055ab225f4eaaa35b116bedbd32d7
Paul Bryan Roberts <NewForester@users.noreply.github.com>
2018-05-27 09:24:01 +0100

    Add another solution to exercism Pangram exercise

--- exercism/pangram.erl
+++ exercism/pangram.erl
@@ -1,15 +1,30 @@
--module(example).
+-module(pangram).
 
 -export([is_pangram/1, test_version/0]).
 
-is_pangram(Sentence) ->
-    Lewand = "zqxjkvbpygfwmucldrhsnioate",      % least common letter first, most common last
+foldl(_, Acc, []) -> {false, Acc};
+foldl(F, Acc, [H|T]) ->
+    case F(H, Acc) of
+        {true, A} -> {true, A};
+        {false, A} -> foldl(F, A, T)
+    end.
 
-    lists:all(fun (A) -> lists:any(fun (L) -> A == string:to_lower(L) end, Sentence) end, Lewand).
+is_pangram(C, S) ->
+    case string:to_lower(C) of
+        L when $a =< L, L =< $z -> test_all_seen(L, S);
+        _ -> {false, S}
+    end.
+
+test_all_seen(L, S) ->
+    S1 = ordsets:add_element(L, S),
+    {ordsets:size(S1) == 26, S1}.
+
+is_pangram(Sentence) ->
+    {R, _} = foldl(fun is_pangram/2, ordsets:new(), Sentence),
+    R.
 
 test_version() -> 1.
 
 %%
-%%  Add Lewand optimisation
+%%  This solution adds a special foldl that allows early return.
 %%
-
